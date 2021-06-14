8968e728e04a5b3d101ae3f447c9ee4d8f340d7b
Wsx <wariostarx@gmail.com>
2021-06-14 15:55:17 +0200

    Add booster areas

---
+++ IndustryProject/Assets/Scripts/SignalPuzzle/SignalRange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignalRange : MonoBehaviour {
+    public bool boostingSignal;
+
+    void Start() {
+        boostingSignal = true;
+    }
+
+    void Update() {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "") {
+            boostingSignal = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.tag == "") {
+            boostingSignal = true;
+        }
+    }
+}
