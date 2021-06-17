99af6e397a2e7b3336ade8b1fda7d03679f01f27
Ben <397710@student.fontys.nl>
2021-06-17 12:54:44 +0200

    Synch led puzzel working

--- IndustryProject/Assets/Scripts/LEDPuzzle/ButtonActive.cs
+++ IndustryProject/Assets/Scripts/LEDPuzzle/ButtonActive.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class ButtonActive : MonoBehaviour
 {
@@ -8,11 +9,12 @@ public class ButtonActive : MonoBehaviour
     public bool finished = false;
     public float timer = 50;
     public GameObject sphereColour;
+    private PhotonView pv;
 
     // Start is called before the first frame update
     void Start()
     {
-        sphereColour.GetComponent<MeshRenderer>().material.color = Color.red;
+        pv = PhotonView.Get(this);
     }
 
     // Update is called once per frame
@@ -20,19 +22,33 @@ public class ButtonActive : MonoBehaviour
     {
         if(finished)
         {
+            pv.RPC("RPCUpdateVar", RpcTarget.All, activated, finished);
+
             sphereColour.GetComponent<MeshRenderer>().material.color = Color.green;
         }
         else if(activated)
         {
             timer -= Time.deltaTime;
+
+            pv.RPC("RPCUpdateVar", RpcTarget.All, activated, finished);
+
             sphereColour.GetComponent<MeshRenderer>().material.color = Color.green;
             if (timer <= 0)
             {
                 activated = false;
+
+                pv.RPC("RPCUpdateVar", RpcTarget.All, activated, finished);
+
                 sphereColour.GetComponent<MeshRenderer>().material.color = Color.red;
                 timer = 10;
-
             }
         }
+    }
+
+    [PunRPC]
+    void RPCUpdateVar(bool one, bool two)
+    {
+        activated = one;
+        finished = two;
     }
 }
